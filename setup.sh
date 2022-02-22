c8820df783f584561710f1d799711445bfb7c9a2
Damodar Lohani <lohanidamodar@users.noreply.github.com>
2022-02-22 22:34:03 +0545

    update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cp $DIR/.profile ~/.profile
+cp $DIR/.bash_profile ~/.bash_profile
 
 cp  -r $DIR/fish/ ~/.config/fish
 sudo chsh -s /usr/bin/fish
