c0d00a52bad2a374add2fb068d1da0b4dc624ad0
Damodar Lohani <dlohani48@gmail.com>
2022-02-24 01:18:52 +0000

    updates

---
+++ install_exa.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# install exa
+export EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
+sudo unzip -q exa.zip bin/exa -d /usr/local
+exa --version
+rm -rf exa.zip
--- setup.sh
+++ setup.sh
@@ -4,5 +4,9 @@ DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cp $DIR/.bash_profile ~/.bash_profile
 
+source ~/.bash_profile
+
+sh $DIR/install_exa.sh
+
 cp  -r $DIR/fish/ ~/.config/fish
 sudo chsh -s /usr/bin/fish
