c85c81b897ba03a008368c2f8d33073c57d4d584
Damodar Lohani <dlohani48@gmail.com>
2022-02-27 06:49:27 +0000

    php 8 exetnsion

--- scripts/install_php8.sh
+++ scripts/install_php8.sh
@@ -4,4 +4,4 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 
 sudo apt update
-sudo apt install -y php8.0 php8.0-mbstring php8.0-dom
+sudo apt install -y php8.0 php8.0-mbstring php8.0-dom php8.0-curl
