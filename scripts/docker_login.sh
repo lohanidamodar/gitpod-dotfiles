c53d6c3966ed6a70a96f09e28ff705206976de97
Damodar Lohani <dlohani48@gmail.com>
2024-09-22 12:26:34 +0000

    docker login

--- scripts/docker_login.sh
+++ scripts/docker_login.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_TOKEN
+# Ensure the necessary environment variables are set
+if [ -z "$DOCKER_HUB_TOKEN" ] || [ -z "$DOCKER_HUB_USERNAME" ]; then
+  echo "Error: DOCKER_HUB_TOKEN or DOCKER_HUB_USERNAME environment variable is not set."
+  exit 1
+fi
+
+# Log in to Docker Hub using the token
+echo "$DOCKER_HUB_TOKEN" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
